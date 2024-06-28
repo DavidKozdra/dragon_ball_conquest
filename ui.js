277aa2f36422797df128c5e9f00daf788bdff159
David <davidkozdra@gmail.com>
2024-06-28 13:28:05 -0400

    fixes to UI issues

--- ui.js
+++ ui.js
@@ -11,14 +11,23 @@ let menus = [
       console.log('Character Selection');
       setGameState('char_select');
     }
+  },
+  {
+    name: 'Settings',
+    image: 'char_select.png',
+    onselect: () => {
+      console.log('settings ');
+      setGameState('settings');
+    }
   }
 ];
 
-let buttonNext, buttonPrevious, buttonSelect, buttonAddPlayer1, buttonAddPlayer2, buttonPlay, buttonBack;
+let buttonNext, buttonPrevious, buttonSelect, buttonAddPlayer1, buttonAddPlayer2, buttonPlay, buttonBack, unpauseButton, startGameButton;
+let player1Div, player2Div;
 
+var Inputs = [];
+var panels = [];
 
-
-var Inputs = []
 function initializeButtons() {
   buttonNext = createButton('>');
   buttonNext.class('game-button');
@@ -41,13 +50,20 @@ function initializeButtons() {
     menus[currentMenu].onselect();
   });
 
-  buttonSelect = createButton('Start Game');
+  buttonSelect = createButton('Re-Start Game');
   buttonSelect.class('game-button');
   buttonSelect.size(100, 50);
   buttonSelect.mousePressed(() => {
     if (selectedCharacters[0] && selectedCharacters[1] && selectedCharacters[0].length > 0 && selectedCharacters[1].length > 0) {
       startGame();
     }
+  });
+
+  unpauseButton = createButton('unPause');
+  unpauseButton.class('game-button');
+  unpauseButton.size(100, 50);
+  unpauseButton.mousePressed(() => {
+    setGameState('playing');
   });
 
   buttonAddPlayer1 = createButton('Add Player 1');
@@ -67,28 +83,45 @@ function initializeButtons() {
     setGameState('main_menu');
   });
 
-  Inputs.push({object: buttonNext, valid_game_state: 'main_menu', name: 'Next'});
-  Inputs.push({object: buttonPrevious, valid_game_state: 'main_menu', name: 'Previous'});
-  Inputs.push({object: buttonPlay, valid_game_state: 'main_menu', name: 'Play'});
-  Inputs.push({object: buttonSelect, valid_game_state: 'char_select', name: 'Select'});
-  Inputs.push({object: buttonAddPlayer1, valid_game_state: 'char_select', name: 'Add Player 1'});
-  Inputs.push({object: buttonAddPlayer2, valid_game_state: 'char_select', name: 'Add Player 2'});
-  Inputs.push({object: buttonBack, valid_game_state: 'char_select', name: 'Back'});
-  Inputs.push({object: buttonSelect, valid_game_state: 'paused', name: 'Back to Main Menu'});
+    startGameButton = createButton('Start Game');
+    startGameButton.class('game-button');
+    startGameButton.size(100, 50);
+    startGameButton.disabled =  selectedCharacters[0] && selectedCharacters[1] && selectedCharacters[0].length > 0 && selectedCharacters[1].length > 0;
+    startGameButton.mousePressed(() => {
+        if (selectedCharacters[0] && selectedCharacters[1] && selectedCharacters[0].length > 0 && selectedCharacters[1].length > 0) {
+            startGame();
+        }
+    });
+
+
+
+  Inputs.push({ object: buttonNext, valid_game_state: 'main_menu', name: 'Next' });
+  Inputs.push({ object: buttonPrevious, valid_game_state: 'main_menu', name: 'Previous' });
+  Inputs.push({ object: buttonPlay, valid_game_state: 'main_menu', name: 'Play' });
+  Inputs.push({ object: startGameButton, valid_game_state: 'char_select', name: 'Start_Game' });
+
+  Inputs.push({ object: buttonAddPlayer1, valid_game_state: 'char_select', name: 'Add Player 1' });
+  Inputs.push({ object: buttonAddPlayer2, valid_game_state: 'char_select', name: 'Add Player 2' });
+  Inputs.push({ object: buttonBack, valid_game_state: 'char_select', name: 'Back' });
+  Inputs.push({ object: buttonSelect, valid_game_state: 'paused', name: 'Back to Main Menu' });
+  Inputs.push({ object: unpauseButton, valid_game_state: 'paused', name: 'unPause' });
+
   // Position buttons initially
   positionButtons();
 }
 
 function positionButtons() {
-    const canvas = document.getElementById('game-canvas');
-    const rect = canvas.getBoundingClientRect();
+  const canvas = document.getElementById('game-canvas');
+  const rect = canvas.getBoundingClientRect();
   buttonNext.position(rect.right - 60, rect.top + rect.height / 2 - 25);
   buttonPrevious.position(rect.left + 10, rect.top + rect.height / 2 - 25);
-  buttonSelect.position(rect.left + rect.width / 2 - 50, rect.bottom + 20);
+  buttonSelect.position(rect.left + rect.width / 2 - 50, rect.bottom - 190);
   buttonPlay.position(rect.left + rect.width / 2 - 50, rect.bottom - 220);
-  buttonAddPlayer1.position(canvasWidth / 4 - 50, canvasHeight / 2);
-  buttonAddPlayer2.position(3 * canvasWidth / 4 - 50, canvasHeight / 2);
+  buttonAddPlayer1.position(rect.x, rect.bottom / 2 + 200);
+  buttonAddPlayer2.position(rect.x + 300, rect.bottom / 2 + 200);
   buttonBack.position(rect.left + 10, rect.bottom - 60);
+  unpauseButton.position(rect.left + rect.width / 2 - 50, rect.bottom - 100);
+  startGameButton.position(rect.left + rect.width / 2 - 50, rect.bottom - 150);
 }
 
 function RenderMainMenu() {
@@ -99,9 +132,6 @@ function RenderMainMenu() {
   textAlign(CENTER, CENTER);
   text('Dragon Ball Conquest', canvasWidth / 2, canvasHeight / 2 - 50);
   textSize(16);
-  let buttonNext =findInputByName('Next');
-  let buttonPrevious =findInputByName('Previous');
-  let buttonPlay =findInputByName('Play');    
   let menu = menus[currentMenu];
   fill(0);
   textSize(16);
@@ -109,9 +139,7 @@ function RenderMainMenu() {
   text(menu.name, canvasWidth / 2, canvasHeight / 2 + 50);
   rect(canvasWidth / 2 - 50, canvasHeight / 2 + 60, 100, 100);
 
-  buttonNext.show();
-  buttonPrevious.show();
-  buttonPlay.show();
+  UpdateUI();
   positionButtons();
 }
 
@@ -124,16 +152,14 @@ function RenderCharacterSelector() {
   text('Select Your Characters', canvasWidth / 2, canvasHeight / 2 - 100);
   textSize(16);
 
-  // Display player 1 and player 2 selected characters as buttons
-  fill(0);
-  textAlign(LEFT, CENTER);
-  text('Player 1:', canvasWidth / 4 - 50, canvasHeight / 2 - 150);
-  const player1Div = createDiv();
-  player1Div.position(canvasWidth / 4 - 50, canvasHeight / 2 - 130);
-  player1Div.style('width', '200px');
+  // Create player 1 div
+  player1Div = createDiv();
+  player1Div.position(rect.width / 4 - 50, rect.height / 2 - 130);
+  player1Div.style('width', rect.width / 2 + 'px');
   player1Div.style('height', '50px');
   player1Div.style('overflow-x', 'scroll');
   player1Div.style('display', 'flex');
+  panels.push({ object: player1Div, valid_game_state: 'char_select', name: 'player1Div' });
 
   if (selectedCharacters[0]) {
     selectedCharacters[0].forEach((char, index) => {
@@ -142,19 +168,18 @@ function RenderCharacterSelector() {
       charButton.mousePressed(() => displayCharacterSelectionPanel(0, index));
       player1Div.child(charButton);
 
-        Inputs.push({object: charButton, valid_game_state: 'char_select', name: char.name});
+      Inputs.push({ object: charButton, valid_game_state: 'char_select', name: char.name });
     });
   }
 
-  fill(0);
-  textAlign(LEFT, CENTER);
-  text('Player 2:', 3 * canvasWidth / 4 - 50, canvasHeight / 2 - 150);
-  const player2Div = createDiv();
-  player2Div.position(3 * canvasWidth / 4 - 50, canvasHeight / 2 - 130);
-  player2Div.style('width', '200px');
+  // Create player 2 div
+  player2Div = createDiv();
+  player2Div.position(rect.width / 4 - 150, rect.height / 2 - 130);
+  player2Div.style('width', rect.width / 2 + 'px');
   player2Div.style('height', '50px');
   player2Div.style('overflow-x', 'scroll');
   player2Div.style('display', 'flex');
+  panels.push({ object: player2Div, valid_game_state: 'char_select', name: 'player2Div' });
 
   if (selectedCharacters[1]) {
     selectedCharacters[1].forEach((char, index) => {
@@ -163,19 +188,18 @@ function RenderCharacterSelector() {
       charButton.mousePressed(() => displayCharacterSelectionPanel(1, index));
       player2Div.child(charButton);
 
-        Inputs.push({object: charButton, valid_game_state: 'char_select', name: char.name});
+      Inputs.push({ object: charButton, valid_game_state: 'char_select', name: char.name });
     });
   }
 
-  // Hide navigation buttons (should be done automaticlly through the inputs ? !)
-
-  buttonSelect.show();
+  UpdateUI();
+  UpdatePanels();
   positionButtons();
 }
 
 function displayCharacterSelectionPanel(playerIndex, charIndex) {
   const panel = createDiv();
-  panel.position(0, canvasHeight / 2 + 100);
+  panel.position(0, rect.height / 2 + 100);
   panel.style('width', '100%');
   panel.style('height', '200px');
   panel.style('overflow-x', 'scroll');
@@ -218,12 +242,23 @@ function findInputByName(name) {
   return Inputs.find(input => input.name === name)?.object;
 }
 
-function UpdateUI() {
-  for (let i = 0; i < Inputs.length; i++) {
-    if (gameState === Inputs[i].valid_game_state) {
-      Inputs[i].object.show();
+function UpdateUI(){
+    for (let i = 0; i < Inputs.length; i++) {
+      if (gameState === Inputs[i].valid_game_state) {
+        console.log(Inputs[i].name);
+        Inputs[i].object.show();
+      } else {
+        Inputs[i].object.hide();
+      }
+    }
+}
+
+function UpdatePanels() {
+  for (let i = 0; i < panels.length; i++) {
+    if (gameState === panels[i].valid_game_state) {
+      panels[i].object.removeClass('hidden');
     } else {
-      Inputs[i].object.hide();
+      panels[i].object.addClass('hidden');
     }
   }
 }
@@ -232,5 +267,5 @@ function onWindowResize() {
   positionButtons();
 }
 
-export { currentMenu, menus, displayCharacterSelectionPanel, RenderCharacterSelector, RenderMainMenu, positionButtons, UpdateUI, selectedCharacters, onWindowResize, findInputByName, initializeButtons };
+export { currentMenu, menus, displayCharacterSelectionPanel, RenderCharacterSelector, RenderMainMenu, positionButtons, UpdateUI, UpdatePanels, selectedCharacters, onWindowResize, findInputByName, initializeButtons };
 
